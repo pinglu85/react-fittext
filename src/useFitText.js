e42a4cdf9b53e75dad76d3cbf64a1b8a0cdbbbd1
pinglu85 <luping8576@hotmail.com>
2020-02-05 01:28:39 +0100

    add maxFontSize and minFontSize options. Add typechecking

--- src/useFitText.js
+++ src/useFitText.js
@@ -1,28 +1,35 @@
 import { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const useFitText = kompressor => {
+const useFitText = (kompressor, minSize, maxSize) => {
   const ref = useRef(null);
   const compressor = kompressor || 1;
+
   useEffect(() => {
-    const minFontSize = Number.NEGATIVE_INFINITY;
-    const maxFontSize = Number.POSITIVE_INFINITY;
+    const minFontSize = minSize || Number.NEGATIVE_INFINITY;
+    const maxFontSize = maxSize || Number.POSITIVE_INFINITY;
     const fit = () => {
-      if (ref.current) {
-        const size = Math.max(
-          Math.min(
-            ref.current.clientWidth / (compressor * 10),
-            parseFloat(maxFontSize)
-          ),
-          parseFloat(minFontSize)
-        );
-        ref.current.style.fontSize = `${size}px`;
-      }
+      const size = Math.max(
+        Math.min(
+          ref.current.clientWidth / (compressor * 10),
+          parseFloat(maxFontSize)
+        ),
+        parseFloat(minFontSize)
+      );
+      ref.current.style.fontSize = `${size}px`;
     };
     fit();
     window.addEventListener('resize', fit);
     return () => window.removeEventListener('resize', fit);
-  }, [ref, compressor]);
+  }, [ref, compressor, minSize, maxSize]);
+
   return [ref];
+};
+
+useFitText.propTypes = {
+  kompressor: PropTypes.number,
+  minSize: PropTypes.number,
+  maxSize: PropTypes.number
 };
 
 export default useFitText;
