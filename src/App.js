ab38270fb8b1e3f849754dc1e11b194c02f8e0c7
pinglu85 <luping8576@hotmail.com>
2020-02-07 12:46:12 +0100

    add second heading

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import useFitText from './useFitText';
 import './App.css';
 
 function App() {
-  const [ref] = useFitText(1.776);
+  const headerRef = useFitText(1.104);
+  const mainRef = useFitText(1.776);
   return (
     <div className="App">
       <header className="App-header">
-        <div className="Heading-wrapper">
-          <h1 ref={ref}>
-            Lorem ipsum dolor sit, conse ctetur adipiscing elit ut aliquam.
-          </h1>
+        <div className="Header-heading-wrapper">
+          <h1 ref={headerRef}>Lorem ipsum dolor sit amet consetus</h1>
         </div>
       </header>
+      <main className="App-main">
+        <div className="Main-heading-wrapper">
+          <h2 ref={mainRef}>
+            Lorem ipsum dolor sit, conse ctetur adipiscing elit ut aliquam.
+          </h2>
+        </div>
+      </main>
     </div>
   );
 }
