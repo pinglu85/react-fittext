997630ddc16b987a7a43f99900e53b33ddd82c87
pinglu85 <luping8576@hotmail.com>
2020-02-08 02:17:21 +0100

    add ReactFitText component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import useFitText from './useFitText';
+import ReactFitText from './ReactFitText';
 import './App.css';
 
 function App() {
@@ -10,15 +10,30 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="Header-heading-wrapper">
-          <h1 ref={headerRef}>Lorem ipsum dolor sit amet consetus</h1>
+          <h1 ref={headerRef} className="Header-heading">
+            Lorem ipsum dolor sit amet consetus
+          </h1>
         </div>
+        <ReactFitText compressor={1.104} width={'38.33%'}>
+          <h1 className="Header-heading" style={{ color: 'tomato' }}>
+            Lorem ipsum dolor sit amet consetus
+          </h1>
+          <h1 className="Header-heading" style={{ color: 'cyan' }}>
+            Lorem ipsum dolor sit amet consetus
+          </h1>
+        </ReactFitText>
       </header>
       <main className="App-main">
         <div className="Main-heading-wrapper">
-          <h2 ref={mainRef}>
+          <h2 ref={mainRef} className="Main-heading">
             Lorem ipsum dolor sit, conse ctetur adipiscing elit ut aliquam.
           </h2>
         </div>
+        <ReactFitText compressor={1.776} width={'51.8%'}>
+          <h2 className="Main-heading" style={{ color: 'tomato' }}>
+            Lorem ipsum dolor sit, conse ctetur adipiscing elit ut aliquam.
+          </h2>
+        </ReactFitText>
       </main>
     </div>
   );
